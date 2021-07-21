10c90ab16cf48023aa465c18fc9d7a1f1fdea412
william <willhgreen02@gmail.com>
2021-07-21 11:08:48 -0400

    commit tree dc287688d22af8910f1ad20469ccedb6a7afebca

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+    return function () {
+        const drivers = ['Sally', 'Bob']
+    }
+}
+
+
+           
+
+
+function returnLastTwoDrivers() {
+    return ['Freddy' , 'Claudia']
+}
+function  selectingDrivers() {
+    return function returnFirstTwoDrivers() {
+
+    }
+}
